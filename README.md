46a0aff7dd0b5cf22004c65c787826e8fb379bac
David Hérault <dherault@gmail.com>
2024-08-28 11:30:44 +0300

    Use lets encrypt to encrypt app engine ssl certificate

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # dev-folio
+
 Portfolios for developers done right
+
+```bash
+certbot certonly --manual --key-type rsa
+cd /etc/letsencrypt/live/dev-folio.com
+openssl rsa -in ./privkey.pem -out ./privkey.pem.rsa.key -traditional
+cat privkey.pem.rsa.key | pbcopy
+cat fullchain.pem | pbcopy
+```
--- dev-folio.com-app-engine/src/server.js
+++ dev-folio.com-app-engine/src/server.js
@@ -16,14 +16,24 @@ app.get('/', async (req, res) => {
     return
   }
 
-  const [indexDotHtml] = await bucket.file('index.html').download()
+  const indexDotHtml = bucket.file('index.html')
+  const [indexDotHtmlExists] = await indexDotHtml.exists()
+
+  if (!indexDotHtmlExists) {
+    res.status(404).send('Project files not found') // TODO 404 page
+
+    return
+  }
+
+  const [indexDotHtmlBuffer] = await indexDotHtml.download()
 
   res.setHeader('content-type', 'text/html')
-  res.send(indexDotHtml)
+  res.send(indexDotHtmlBuffer)
 })
 
 // Listen to the App Engine-specified port, or 8080 otherwise
 const PORT = process.env.PORT || 8080
+
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}...`)
 })
