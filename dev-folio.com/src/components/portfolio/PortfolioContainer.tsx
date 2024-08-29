99839b8a4c12fb475d73f06ef08f6988ddc7c560
David Hérault <dherault@gmail.com>
2024-08-29 14:21:43 +0300

    Design PortfolioContainer

--- dev-folio.com/src/components/portfolio/PortfolioContainer.tsx
+++ dev-folio.com/src/components/portfolio/PortfolioContainer.tsx
@@ -3,12 +3,20 @@ import { motion } from 'framer-motion'
 import _ from 'clsx'
 
 import usePortfolio from '~hooks/portfolio/usePortfolio'
+import useWindowSize from '~hooks/common/useWindowSize'
 
 import PortfolioEditor from '~components/portfolio/editor/PortfolioEditor'
 
 function PortfolioContainer({ children }: PropsWithChildren) {
   const { edited } = usePortfolio()
   const initialEdited = useRef(edited)
+
+  const { width: windowWidth } = useWindowSize()
+
+  const contentWidth = windowWidth - 512 - 3 * 32
+  const scale = windowWidth ? contentWidth / windowWidth : 1
+
+  console.log('contentWidth/windowWidth', contentWidth, scale)
 
   return (
     <div className="relative grow overflow-x-hidden overflow-y-auto bg-neutral-background">
@@ -38,14 +46,12 @@ function PortfolioContainer({ children }: PropsWithChildren) {
         animate={edited ? 'open' : 'close'}
         variants={{
           open: {
-            scale: 0.5,
-            right: -256,
+            right: 32,
             transition: {
               ease: 'easeInOut',
             },
           },
           close: {
-            scale: 1,
             right: 0,
             transition: {
               ease: 'easeInOut',
@@ -54,14 +60,38 @@ function PortfolioContainer({ children }: PropsWithChildren) {
         }}
         className="absolute top-0 z-10"
       >
-        <div
-          className={_('max-w-[1400px] bg-white border transition-colors', {
-            'border-transparent': !edited,
-          })}
+        <motion.div
+          initial={initialEdited ? 'open' : 'close'}
+          animate={edited ? 'open' : 'close'}
+          variants={{
+            open: {
+              scale,
+              transition: {
+                ease: 'easeInOut',
+              },
+            },
+            close: {
+              scale: 1,
+              transition: {
+                ease: 'easeInOut',
+              },
+            },
+          }}
+          className="origin-right"
         >
-          {children}
-        </div>
+          <div
+            className={_('max-w-[1400px] bg-white border transition-colors', {
+              'border-transparent': !edited,
+            })}
+          >
+            {children}
+          </div>
+        </motion.div>
       </motion.div>
+      <div
+        style={{ width: contentWidth, marginLeft: 512 + 2 * 32 }}
+        className="h-1 bg-neutral-200"
+      />
     </div>
   )
 }
